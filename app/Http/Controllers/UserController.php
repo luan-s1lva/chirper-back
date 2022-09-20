e8d15480dad1d1756c8407e8cb3b751a5869345b
Luan Silva <l.carmmo141@gmail.com>
2022-09-20 11:17:25 -0300

    Lógica de cadastro do usuário implementada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller 
 {
-    
+    public function storeUser(Request $request) {
+        $user = new User;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+
+        $user->save();
+    }
 }
